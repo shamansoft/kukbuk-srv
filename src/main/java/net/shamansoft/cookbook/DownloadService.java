5d6d95720b9d2fda11060937f65c51180358366d
Alexey <25253567+khisamutdinov@users.noreply.github.com>
2025-02-14 22:44:27 -0800

    feat: add DownloadService and configure build

---
+++ src/main/java/net/shamansoft/cookbook/DownloadService.java
@@ -0,0 +1,12 @@
+package net.shamansoft.cookbook;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DownloadService {
+
+    public String fetch(String url) {
+        System.out.println("fetch");
+        return "";
+    }
+}
