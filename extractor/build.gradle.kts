58493746297cfd8c4a6727cceee1de67e08175de
Alexey <25253567+khisamutdinov@users.noreply.github.com>
2026-03-19 21:21:23 -0700

    bump version

--- extractor/build.gradle.kts
+++ extractor/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     alias(libs.plugins.owasp.dependency.check)
 }
 
-version = "0.14.0-SNAPSHOT"
+version = "0.15.0-SNAPSHOT"
 
 springBoot {
     buildInfo()   // This will generate a build-info.properties file with accurate values
